31b254df84de4ded0590594a116cfaa984060119
Devon-Lin <devon.lin@bell.net>
2019-11-11 17:53:43 -0500

    Firebase changes

--- app/src/main/java/com/teama/walkinclinic/ViewShiftScreen.java
+++ app/src/main/java/com/teama/walkinclinic/ViewShiftScreen.java
@@ -1,14 +1,56 @@
 package com.teama.walkinclinic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CalendarView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ViewShiftScreen extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference shifts;
+    CalendarView shiftCalendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_shift_screen);
+
+        database = FirebaseDatabase.getInstance();
+        shifts = database.getReference("Users").child("employee");
+
+        shiftCalendar = findViewById(R.id.cvShifts);
+
+        shiftCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Shift shift = new Shift(String.valueOf(month),String.valueOf(dayOfMonth),String.valueOf(10));
+
+            }
+        });
+
+
+
+
     }
+
 }

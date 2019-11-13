663fdc6993d7298e44c7d790dd223081f71ec738
kprime21 <55168056+kprime21@users.noreply.github.com>
2019-11-13 13:57:29 -0500

    changed the operating hour system again
    -Predefined operating hours defined through Spinner
    
    -Set Hours will let employee add hours depending on spinner choice
    -Need to a toast that if save changes, will erase all current shift hours

--- app/src/main/java/com/teama/walkinclinic/ClinicOperatingHours.java
+++ app/src/main/java/com/teama/walkinclinic/ClinicOperatingHours.java
@@ -2,21 +2,28 @@ package com.teama.walkinclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.app.AlertDialog;
+
 
 public class ClinicOperatingHours extends AppCompatActivity {
+    AlertDialog adgSaveChanges;
     Button btnSaveClinicInfoChanges;
+    Button btnGoToSetMyHours;
+    Spinner spOperatingHours;
     int startHourOfClinic;
     int closeHourOfClinic;
-    NumberPicker npClinicOpeningHours;
-    NumberPicker npClinicClosingHours;
-    Spinner spinner2;
-    Spinner spinner3;
+    String hoursOperating;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,51 +33,82 @@ public class ClinicOperatingHours extends AppCompatActivity {
         setContentView(R.layout.activity_clinic_operating_hours);
 
         btnSaveClinicInfoChanges = findViewById(R.id.btnSaveClinicInfoChanges);
+        btnGoToSetMyHours = findViewById(R.id.btnGoToSetMyHours);
 
+        spOperatingHours = findViewById(R.id.spOperatingHours);
+        hoursOperating = spOperatingHours.getSelectedItem().toString();
 
-        spinner2 = findViewById(R.id.spinner2);
-        spinner3 = findViewById(R.id.spinner3);
+        adgSaveChanges = new AlertDialog.Builder(ClinicOperatingHours.this).create();
 
-        npClinicOpeningHours = findViewById(R.id.npClinicOpeningHours);
-        npClinicClosingHours = findViewById(R.id.npClinicClosingHours);
-
-        //Populate opening/closing with 1-12
-        npClinicOpeningHours.setMinValue(1);
-        npClinicOpeningHours.setMaxValue(12);
-        npClinicClosingHours.setMinValue(1);
-        npClinicClosingHours.setMaxValue(12);
-        //wrap when reach max
-        npClinicOpeningHours.setWrapSelectorWheel(true);
-        npClinicClosingHours.setWrapSelectorWheel(true);
-
-        npClinicOpeningHours.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        spOperatingHours.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                    startHourOfClinic = newVal;
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                hoursOperating = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
-        npClinicClosingHours.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                    closeHourOfClinic = newVal;
-            }
-        });
+
+
+
 
         btnSaveClinicInfoChanges.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int timeIntervalEnd = startHourOfClinic;
-                int timeIntervalStart = closeHourOfClinic;
-                if(spinner2.getSelectedItem().toString().equals("PM")){timeIntervalStart+=10;}
-                if(spinner3.getSelectedItem().toString().equals("PM")){timeIntervalEnd+=10;}
-                if(Math.abs(timeIntervalEnd-timeIntervalStart) <8){
-                    Toast.makeText(getApplicationContext(), "The Clinic must be open at least 8 hours", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "The Clinic hours have been updated", Toast.LENGTH_SHORT).show();
-                }
+
+                adgSaveChanges.setTitle("Warning");
+                adgSaveChanges.setMessage("If you change the operating hours, it will erase all current employee shifts!");
+                adgSaveChanges.setButton(adgSaveChanges.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if(hoursOperating.equals("Normal Hours: 8AM - 8PM")){
+                            startHourOfClinic = 8;
+                            closeHourOfClinic = 20;
+                            hoursOperating = "8AM - 8PM";
+
+                        }
+                        else if(hoursOperating.equals("Holiday Hours: 10AM - 2PM")){
+                            startHourOfClinic = 10;
+                            closeHourOfClinic = 14;
+                            hoursOperating = "10AM - 2PM";
+                        }
+                        else{
+                            startHourOfClinic = 24;
+                            closeHourOfClinic = 24;
+                            hoursOperating = "24 hours";
+                        }
+                        Toast.makeText(getApplicationContext(),"The hours of the clinic have been set to " + hoursOperating, Toast.LENGTH_LONG ).show();
+                    }
+                });
+                adgSaveChanges.setButton(adgSaveChanges.BUTTON_POSITIVE, "CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                adgSaveChanges.show();
+                adgSaveChanges.getButton(AlertDialog.BUTTON_NEUTRAL).setTextColor(Color.BLACK);
+                adgSaveChanges.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.BLACK);
+
             }
         });
+
+        btnGoToSetMyHours.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent s = new Intent(getApplicationContext(),WorkDaysScreen.class);
+                s.putExtra("clinicStartHours",startHourOfClinic);
+                s.putExtra("clinicCloseHours",closeHourOfClinic);
+                s.putExtra("uidemployee",uidemployee);
+                startActivity(s);
+            }
+        });
+
+
 
     }
 }
@@ -117,3 +155,56 @@ TimePickerDialog timePickerDialog;
 
 
 */
+/*
+Old NumberPicker
+ btnSaveClinicInfoChanges = findViewById(R.id.btnSaveClinicInfoChanges);
+
+        import android.widget.NumberPicker;
+        NumberPicker npClinicOpeningHours;
+        NumberPicker npClinicClosingHours;
+        Spinner spinner2;
+        Spinner spinner3;
+        spinner2 = findViewById(R.id.spinner2);
+        spinner3 = findViewById(R.id.spinner3);
+
+        npClinicOpeningHours = findViewById(R.id.npClinicOpeningHours);
+        npClinicClosingHours = findViewById(R.id.npClinicClosingHours);
+
+        //Populate opening/closing with 1-12
+        npClinicOpeningHours.setMinValue(1);
+        npClinicOpeningHours.setMaxValue(12);
+        npClinicClosingHours.setMinValue(1);
+        npClinicClosingHours.setMaxValue(12);
+        //wrap when reach max
+        npClinicOpeningHours.setWrapSelectorWheel(true);
+        npClinicClosingHours.setWrapSelectorWheel(true);
+
+        npClinicOpeningHours.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                    startHourOfClinic = newVal;
+            }
+        });
+        npClinicClosingHours.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                    closeHourOfClinic = newVal;
+            }
+        });
+
+        btnSaveClinicInfoChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int timeIntervalEnd = startHourOfClinic;
+                int timeIntervalStart = closeHourOfClinic;
+                if(spinner2.getSelectedItem().toString().equals("PM")){timeIntervalStart+=10;}
+                if(spinner3.getSelectedItem().toString().equals("PM")){timeIntervalEnd+=10;}
+                if(Math.abs(timeIntervalEnd-timeIntervalStart) <8){
+                    Toast.makeText(getApplicationContext(), "The Clinic must be open at least 8 hours", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "The Clinic hours have been updated", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+ */

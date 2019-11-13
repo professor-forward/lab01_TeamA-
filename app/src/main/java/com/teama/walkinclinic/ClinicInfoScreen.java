59215b3748434a98e00efab53af3a9103232a94d
kprime21 <55168056+kprime21@users.noreply.github.com>
2019-11-12 20:09:23 -0500

    Added TimePickerDialog

--- app/src/main/java/com/teama/walkinclinic/ClinicInfoScreen.java
+++ app/src/main/java/com/teama/walkinclinic/ClinicInfoScreen.java
@@ -1,31 +1,27 @@
 package com.teama.walkinclinic;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class ClinicInfoScreen extends AppCompatActivity {
 
-
+    TimePickerDialog timePickerDialog;
+    EditText edtClinicOpeningHours;
+    EditText edtClinicClosingHours;
     Button btnGoToSetMyHours;
+    Button btnSaveClinicInfoChanges;
+
+    String amOrPm;
+    int StartHourOfDay;
+    int EndHourOfDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +29,53 @@ public class ClinicInfoScreen extends AppCompatActivity {
         setContentView(R.layout.activity_clinic_info_screen);
 
         btnGoToSetMyHours = findViewById(R.id.btnGoToSetMyHours);
+        btnSaveClinicInfoChanges = findViewById(R.id.btnSaveClinicInfoChanges);
+        edtClinicOpeningHours = findViewById(R.id.edtClinicOpeningHours);
+        edtClinicClosingHours = findViewById(R.id.edtClinicClosingHours);
+
+        edtClinicOpeningHours.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 timePickerDialog = new TimePickerDialog(ClinicInfoScreen.this, R.style.TimePickerTheme, new TimePickerDialog.OnTimeSetListener() {
+                     @Override
+                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                         StartHourOfDay = hourOfDay;
+                         if(hourOfDay>=12){
+                             amOrPm = "PM";
+                         }
+                         else{
+                             amOrPm = "AM";
+                         }
+                         edtClinicOpeningHours.setText(String.format("%02d:%02d", hourOfDay, minute) + " " + amOrPm);
+                     }
+                 },0,0,false);
+                 timePickerDialog.show();
+            }
+        });
+
+        edtClinicClosingHours.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePickerDialog = new TimePickerDialog(ClinicInfoScreen.this, R.style.TimePickerTheme, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        EndHourOfDay = hourOfDay;
+                        if(hourOfDay>=12){
+                            amOrPm = "PM";
+                        }
+                        else{
+                            amOrPm = "AM";
+                        }
+                        edtClinicClosingHours.setText(String.format("%02d:%02d", hourOfDay, minute) + " " + amOrPm);
+                    }
+                },0,0,false);
+                timePickerDialog.show();
+            }
+        });
+
+
 
         final String uidemployee = getIntent().getExtras().getString("uidemployee");
-
         btnGoToSetMyHours.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +85,17 @@ public class ClinicInfoScreen extends AppCompatActivity {
 
             }
         });
+
+        btnSaveClinicInfoChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Math.abs(EndHourOfDay - StartHourOfDay) <8){
+                    Toast.makeText(getApplicationContext(), "The Clinic must be open at least 8 hours", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
 
 

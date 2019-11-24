026332acd8c2c676097de00179fb9748104c2d06
Devon-Lin <devon.lin@bell.net>
2019-11-23 20:04:12 -0500

    Review page frontend finished

--- app/src/main/java/com/teama/walkinclinic/ReviewScreen.java
+++ app/src/main/java/com/teama/walkinclinic/ReviewScreen.java
@@ -3,6 +3,10 @@ package com.teama.walkinclinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ReviewScreen extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class ReviewScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_screen);
+
+        final RatingBar starBar = (RatingBar) findViewById(R.id.ratingBar);
+        Button submitButton = (Button) findViewById(R.id.submitBtn);
+        final TextView ratingText  = (TextView) findViewById(R.id.ratingTextView);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ratingText.setText("You Selected " + starBar.getRating());
+            }
+        });
     }
 }
